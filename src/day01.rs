8980e65a47ef4832e73457c90b85ca44757c5281
Basile Henry <bjm.henry@gmail.com>
2020-12-01 07:12:19 +0100

    Day 1 solutions

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,69 @@
-pub fn solve(_input: &str, _part: u8) -> () {
-    println!("{}", _input);
-    println!("{}", _part);
+use std::io;
+use std::io::prelude::*;
+use std::io::{Error, ErrorKind};
+
+use std::collections::BTreeSet;
+
+pub fn solve(input: impl BufRead, part: u8) -> io::Result<()> {
+    match part {
+        1 => part_1(input)?,
+        2 => part_2(input)?,
+        _ => unimplemented!(),
+    }
+
+    Ok(())
+}
+
+pub fn part_1(input: impl BufRead) -> io::Result<()> {
+    let mut expenses = BTreeSet::new();
+
+    for line in input.lines() {
+        let expense = line?
+            .parse::<i32>()
+            .map_err(|e| Error::new(ErrorKind::InvalidInput, e))?;
+
+        let other = 2020 - expense;
+
+        // Find answer
+        if expenses.contains(&other) {
+            println!("{} * {} = {}", expense, other, expense * other);
+
+            return Ok(());
+        }
+
+        expenses.insert(expense);
+    }
+
+    Ok(())
+}
+
+pub fn part_2(input: impl BufRead) -> io::Result<()> {
+    let mut expenses = BTreeSet::new();
+
+    for line in input.lines() {
+        let expense = line?
+            .parse::<i32>()
+            .map_err(|e| Error::new(ErrorKind::InvalidInput, e))?;
+
+        for entry in &expenses {
+            let other = 2020 - expense - entry;
+
+            // Find answer
+            if expenses.contains(&other) {
+                println!(
+                    "{} * {} * {} = {}",
+                    expense,
+                    entry,
+                    other,
+                    expense * entry * other
+                );
+
+                return Ok(());
+            }
+        }
+
+        expenses.insert(expense);
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::fs::read_to_string;
+use std::fs::File;
 use std::io;
+use std::io::BufReader;
 use structopt::StructOpt;
 
 mod day01;
@@ -15,10 +16,10 @@ fn main() -> io::Result<()> {
     let opt = Opt::from_args();
 
     let input_path = format!("input/day_{:0>2}/part_{}.txt", opt.day, opt.part);
-    let input = read_to_string(input_path)?;
+    let input = BufReader::new(File::open(input_path)?);
 
     match opt.day {
-        1 => day01::solve(&input, opt.part),
+        1 => day01::solve(input, opt.part)?,
         _ => unimplemented!(),
     }
 
