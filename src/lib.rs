04369b6d6fe11a9c64b9f08cfb81199b9f5369e9
Basile Henry <bjm.henry@gmail.com>
2020-12-02 21:54:11 +0100

    Move input files

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::io::BufReader;
 
 pub fn input_file(day: u8) -> io::Result<BufReader<File>> {
-    let input_path = format!("input/day_{:0>2}/input.txt", day);
+    let input_path = format!("inputs/day_{:0>2}.txt", day);
 
     Ok(BufReader::new(File::open(input_path)?))
 }
