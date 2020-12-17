bc3bd2167138def047a6c960063fa5903adbe275
Basile Henry <bjm.henry@gmail.com>
2020-12-17 15:53:27 +0100

    Const generics for Day 17

--- src/day17.rs
+++ src/day17.rs
@@ -1,14 +1,14 @@
 use std::io;
 use std::io::prelude::*;
+use std::ops::Add;
 
 use std::collections::HashSet;
 
 pub fn solve(input: impl BufRead, part: u8) -> io::Result<()> {
-    let input = parse(input);
-
     let solution = match part {
-        1 => part_1(input),
-        2 => part_2(input),
+        1 => part_1(Grid::parse(input)),
+        2 => part_2(Grid::parse(input)),
+        3 => part_3(Grid::parse(input)),
         _ => unimplemented!(),
     };
 
@@ -17,76 +17,137 @@ pub fn solve(input: impl BufRead, part: u8) -> io::Result<()> {
     Ok(())
 }
 
-fn parse(mut input: impl BufRead) -> Grid {
-    let mut input_str = String::new();
-    input.read_to_string(&mut input_str).unwrap();
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct Pos<const N: usize>([isize; N]); // x, y, z, w
 
-    let raw_grid: Vec<Pos> = input_str
-        .lines()
-        .enumerate()
-        .flat_map(|(y, l)| {
-            l.as_bytes()
-                .into_iter()
-                .enumerate()
-                .filter(|(_, cell)| **cell == b'#')
-                .map(move |(x, _)| (x as isize, y as isize, 0isize, 0isize))
-        })
-        .collect();
+impl<const N: usize> Pos<N> {
+    fn repeated(x: isize) -> Self {
+        Self([x; N])
+    }
 
-    let (max_x, max_y, _, _) = *raw_grid.last().unwrap();
-    let grid = raw_grid.into_iter().collect();
+    fn min(&self, other: &Self) -> Self {
+        let mut pos = [0; N];
 
-    Grid {
-        grid,
-        x_bounds: (0, max_x),
-        y_bounds: (0, max_y),
-        z_bounds: (0, 0),
-        w_bounds: (0, 0),
+        for (i, s) in self.0.iter().enumerate() {
+            pos[i] = *s.min(&other.0[i]);
+        }
+
+        Self(pos)
+    }
+
+    fn max(&self, other: &Self) -> Self {
+        let mut pos = [0; N];
+
+        for (i, s) in self.0.iter().enumerate() {
+            pos[i] = *s.max(&other.0[i]);
+        }
+
+        Self(pos)
     }
 }
 
-type Pos = (isize, isize, isize, isize); // x, y, z, w
+impl<const N: usize> Add for Pos<N> {
+    type Output = Self;
 
-#[derive(Debug)]
-struct Grid {
-    grid: HashSet<Pos>, // Only the active cells
-    x_bounds: (isize, isize),
-    y_bounds: (isize, isize),
-    z_bounds: (isize, isize),
-    w_bounds: (isize, isize),
+    fn add(self, other: Self) -> Self {
+        let mut pos = [0; N];
+
+        for (i, pos) in pos.iter_mut().enumerate() {
+            *pos = self.0[i] + other.0[i];
+        }
+
+        Pos(pos)
+    }
 }
 
-impl Grid {
-    fn _print(&self) {
-        for w in self.w_bounds.0..=self.w_bounds.1 {
-            for z in self.z_bounds.0..=self.z_bounds.1 {
-                println!("z={}, w={}", z, w);
-                for y in self.y_bounds.0..=self.y_bounds.1 {
-                    for x in self.x_bounds.0..=self.x_bounds.1 {
-                        let active = self.grid.contains(&(x, y, z, w));
-                        print!("{}", if active { '#' } else { '.' });
-                    }
-                    println!();
+struct PosRange<const N: usize> {
+    start: Pos<N>,
+    end: Pos<N>,
+    current: Option<Pos<N>>,
+}
+
+impl<const N: usize> PosRange<N> {
+    fn new(start: &Pos<N>, end: &Pos<N>) -> Self {
+        Self {
+            start: start.clone(),
+            end: end.clone(),
+            current: None,
+        }
+    }
+}
+
+impl<const N: usize> Iterator for PosRange<N> {
+    type Item = Pos<N>;
+
+    fn next(&mut self) -> Option<Pos<N>> {
+        match &mut self.current {
+            None => self.current = Some(self.start.clone()),
+            Some(c) if *c == self.end => return None,
+            Some(c) => {
+                let mut i = 0;
+
+                while c.0[i] >= self.end.0[i] {
+                    c.0[i] = self.start.0[i];
+                    i += 1;
                 }
-                println!();
+
+                c.0[i] += 1;
             }
+        }
+
+        self.current.clone()
+    }
+}
+
+#[derive(Debug)]
+struct Grid<const N: usize> {
+    grid: HashSet<Pos<N>>, // Only the active cells
+    min_bound: Pos<N>,
+    max_bound: Pos<N>,
+}
+
+impl<const N: usize> Grid<N> {
+    fn parse(mut input: impl BufRead) -> Self {
+        let mut input_str = String::new();
+        input.read_to_string(&mut input_str).unwrap();
+
+        let raw_grid: Vec<Pos<N>> = input_str
+            .lines()
+            .enumerate()
+            .flat_map(|(y, l)| {
+                l.as_bytes()
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, cell)| **cell == b'#')
+                    .map(move |(x, _)| {
+                        let mut pos = [0; N];
+                        pos[0] = x as isize;
+                        pos[1] = y as isize;
+                        Pos(pos)
+                    })
+            })
+            .collect();
+
+        let max_bound = raw_grid.last().unwrap().clone();
+        let grid = raw_grid.into_iter().collect();
+
+        Grid {
+            grid,
+            min_bound: Pos::repeated(0),
+            max_bound,
         }
     }
 
-    fn neighbours_count(&self, &(x, y, z, w): &Pos) -> usize {
+    fn neighbours_count(&self, pos: &Pos<N>) -> usize {
         let mut count = 0;
+        let zero = Pos::repeated(0);
+        let iter = PosRange::new(&Pos::repeated(-1), &Pos::repeated(1));
 
-        for dw in -1..=1 {
-            for dz in -1..=1 {
-                for dy in -1..=1 {
-                    for dx in -1..=1 {
-                        if (dx, dy, dz, dw) != (0, 0, 0, 0) {
-                            let active = self.grid.contains(&(x + dx, y + dy, z + dz, w + dw));
-                            if active {
-                                count += 1;
-                            }
-                        }
-                    }
+        for dpos in iter {
+            if dpos != zero {
+                let active = self.grid.contains(&(pos.clone() + dpos));
+                if active {
+                    count += 1;
                 }
             }
         }
@@ -94,68 +155,57 @@ impl Grid {
         count
     }
 
-    fn step(&self, dims_4: bool) -> Grid {
-        let mut x_bounds = (0, 0);
-        let mut y_bounds = (0, 0);
-        let mut z_bounds = (0, 0);
-        let mut w_bounds = (0, 0);
+    fn step(&self) -> Self {
         let mut grid = HashSet::new();
+        let mut min_bound = Pos::repeated(0);
+        let mut max_bound = Pos::repeated(0);
 
-        let w_iter = if dims_4 {
-            (self.w_bounds.0 - 1)..=(self.w_bounds.1 + 1)
-        } else {
-            0..=0
-        };
+        let pos_min_one = Pos::repeated(-1);
+        let pos_plus_one = Pos::repeated(1);
+        let pos_range = PosRange::new(
+            &(self.min_bound.clone() + pos_min_one),
+            &(self.max_bound.clone() + pos_plus_one),
+        );
 
-        for w in w_iter {
-            for z in (self.z_bounds.0 - 1)..=(self.z_bounds.1 + 1) {
-                for y in (self.y_bounds.0 - 1)..=(self.y_bounds.1 + 1) {
-                    for x in (self.x_bounds.0 - 1)..=(self.x_bounds.1 + 1) {
-                        let cell = (x, y, z, w);
-                        let active = self.grid.contains(&cell);
-                        let count = self.neighbours_count(&cell);
+        for pos in pos_range {
+            let active = self.grid.contains(&pos);
+            let count = self.neighbours_count(&pos);
 
-                        let new_active = count == 3 || (active && count == 2);
+            let new_active = count == 3 || (active && count == 2);
 
-                        if new_active {
-                            grid.insert(cell);
-                            x_bounds.0 = x_bounds.0.min(x);
-                            x_bounds.1 = x_bounds.1.max(x);
-                            y_bounds.0 = y_bounds.0.min(y);
-                            y_bounds.1 = y_bounds.1.max(y);
-                            z_bounds.0 = z_bounds.0.min(z);
-                            z_bounds.1 = z_bounds.1.max(z);
-                            w_bounds.0 = w_bounds.0.min(w);
-                            w_bounds.1 = w_bounds.1.max(w);
-                        }
-                    }
-                }
+            if new_active {
+                min_bound = min_bound.min(&pos);
+                max_bound = max_bound.max(&pos);
+                grid.insert(pos);
             }
         }
 
         Grid {
             grid,
-            x_bounds,
-            y_bounds,
-            z_bounds,
-            w_bounds,
+            min_bound,
+            max_bound,
         }
     }
 
-    fn run_steps(&mut self, steps: usize, dims_4: bool) {
+    fn run_steps(&mut self, steps: usize) {
         for _ in 0..steps {
-            *self = self.step(dims_4);
+            *self = self.step();
         }
     }
 }
 
-fn part_1(mut grid: Grid) -> usize {
-    grid.run_steps(6, false);
+fn part_1(mut grid: Grid<3>) -> usize {
+    grid.run_steps(6);
     grid.grid.len()
 }
 
-fn part_2(mut grid: Grid) -> usize {
-    grid.run_steps(6, true);
+fn part_2(mut grid: Grid<4>) -> usize {
+    grid.run_steps(6);
+    grid.grid.len()
+}
+
+fn part_3(mut grid: Grid<5>) -> usize {
+    grid.run_steps(6);
     grid.grid.len()
 }
 
@@ -170,13 +220,19 @@ mod test {
 
     #[test]
     fn part_1_example() {
-        let input = parse(io::Cursor::new(EXAMPLE));
+        let input = Grid::parse(io::Cursor::new(EXAMPLE));
         assert_eq!(part_1(input), 112);
     }
 
     #[test]
     fn part_2_example() {
-        let input = parse(io::Cursor::new(EXAMPLE));
+        let input = Grid::parse(io::Cursor::new(EXAMPLE));
         assert_eq!(part_2(input), 848);
+    }
+
+    #[test]
+    fn part_3_example() {
+        let input = Grid::parse(io::Cursor::new(EXAMPLE));
+        assert_eq!(part_3(input), 5760);
     }
 }
